3db662dd9242aee000f66a300d88e743736f30b9
GM <pixilla20@gmail.com>
2026-05-12 21:19:49 +0900

    chore:github action ci 파이프라인추가

--- src/main/java/portfolio/PortfolioApplication.java
+++ src/main/java/portfolio/PortfolioApplication.java
@@ -8,6 +8,8 @@ public class PortfolioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioApplication.class, args);
+
+		System.out.println("Hello World");
 	}
 
 }
